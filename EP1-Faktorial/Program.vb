e98a78a3e7d9fb850e4f5e2108b3e22bc8b42378
Daniel Fiala <Fiala.Daniel@stud.vassboskovice.cz>
2024-05-13 12:33:09 +0200

    Začátek

--- EP1-Faktorial/Program.vb
+++ EP1-Faktorial/Program.vb
@@ -2,6 +2,17 @@ Imports System
 
 Module Program
   Sub Main(args As String())
-    Console.WriteLine("Hello World!")
-  End Sub
+        Dim cislo As Integer
+        Dim vysledek As Integer
+
+        If cislo = String Then
+            Console.WriteLine(" Vstup musí být èíslo ")
+        End If
+
+        If cislo > 10 Then
+            Console.WriteLine(" èíslo musí být menší ")
+        End If
+
+
+    End Sub
 End Module
