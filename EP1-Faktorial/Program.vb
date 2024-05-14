04fc2f6a2fb7cecd44378301c417f7fb6bbe48c7
dan <danik.fiala@email.cz>
2024-05-14 20:00:48 +0200

    Finální zakončení

--- EP1-Faktorial/Program.vb
+++ EP1-Faktorial/Program.vb
@@ -5,34 +5,34 @@ Module Program
     Sub Main(args As String())
         Dim cislo As Object
         Dim vysledek As Integer = 1
-        Do
-            Console.WriteLine("Zadej èíslo")
-            cislo = Console.ReadLine
+
+        Console.WriteLine("Zadej èíslo")
+        cislo = Console.ReadLine
 
 
-            If Not IsNumeric(cislo) Then
-                Console.WriteLine(" Vstup musí být èíslo ")
-                Console.ReadKey()
-                End
-            End If
+        If Not IsNumeric(cislo) Then
+            Console.WriteLine(" Vstup musí být èíslo ")
+            Console.ReadKey()
+            End
+        End If
 
-            If cislo > 10 Then
-                Console.WriteLine(" èíslo musí být menší ")
-                Console.ReadKey()
-                End
-            End If
+        If cislo > 10 Then
+            Console.WriteLine(" èíslo musí být menší ")
+            Console.ReadKey()
+            End
+        End If
 
-            If cislo < 0 Then
-                Console.WriteLine(" minimální zadané èíslo musí být 1")
-                Console.ReadKey()
-                End
-            End If
+        If cislo < 0 Then
+            Console.WriteLine(" minimální zadané èíslo musí být 1")
+            Console.ReadKey()
+            End
+        End If
 
 
-            For i As Integer = 1 To cislo
-                vysledek *= i
-            Next
-        Loop
+        For i As Integer = 1 To cislo
+            vysledek *= i
+        Next
+
 
 
         Console.WriteLine($"faktoriál zadaného èísla {cislo} je {vysledek}")
